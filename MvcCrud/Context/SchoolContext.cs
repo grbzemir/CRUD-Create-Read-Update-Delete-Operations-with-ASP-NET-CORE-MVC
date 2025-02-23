1dd02cb12836fe81959fac0522a963045f9c98c4
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-02-24 00:36:56 +0300

    Add project files.

---
+++ MvcCrud/Context/SchoolContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using MvcCrud.Models;
+
+namespace MvcCrud.Context
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students {get; set;}
+    }
+}
